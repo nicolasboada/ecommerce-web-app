2ec64d86d52c6534c2f2e282cdb367d9377e26b0
Nicolas Boada <nicolasboada@yahoo.com.ar>
2022-02-15 20:33:55 -0300

    add path import

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 // const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
+const path = require("path");
 
 mongoose
   .connect(process.env.MONGO_URL)
